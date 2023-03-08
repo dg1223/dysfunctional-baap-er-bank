d7d9c3719ed1921efdc7ad096c0e4caaeef25a60
dg1223 <alavi1223@hotmail.com>
2023-03-07 19:40:19 -0500

    final

---
+++ js/__debug__steps.js
@@ -0,0 +1,7 @@
+/*
+Debug steps
+1. Go to the live website and try to reproduce the error
+2, Check the console of the live website to find any errors
+3. Read exception thoroughly
+4. If the error is new to you, copy it and search online
+*/
